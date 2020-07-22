96b98c317b1f54b47f7a9a5a7c985bc5a75ea48d
devmallett <34248685+devmallett@users.noreply.github.com>
2020-07-22 16:30:59 -0400

    ioncon added

---
+++ zip-info/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+// import VueRouter from 'vue-router'
+import { IonicVueRouter } from '@ionic/vue';
+
+import Home from '../views/Home.vue';
+
+Vue.use(IonicVueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  }
+]
+
+const router = new IonicVueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
